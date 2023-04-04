41d0b8c66c90f46a7264e1104107c369f247d7b1
B4Lee <iqbalanggoro48@gmail.com>
2023-04-05 00:57:32 +0700

    update Logo & movie to home redirection

--- src/components/Header/NavBar.vue
+++ src/components/Header/NavBar.vue
@@ -1,22 +1,24 @@
 <template>
-    <div class="flex mt-5">
-     <img src="../../assets/images/logo.svg" alt="" class="h-12 max-h-full ">
-     <ul class="flex py-3 space-x-4">
-        <li>
-            <router-link to="/movies">
-                Movies
-            </router-link>
-        </li>
-        <li>
-            <router-link to="/actors">
-                Actors
-            </router-link>
-        </li>
-        <li>
-            <router-link to="/profile">
-                Profile
-            </router-link>
-        </li>
-     </ul>
+    <div class="flex mt-5 px-4">
+        <router-link to="/">
+            <img src="https://delphiy.github.io/movies-hunter/img/logo.147b0df4.png" alt="" class="h-12 max-h-full ">
+        </router-link>
+        <ul class="flex py-3 space-x-4 ml-4">
+            <li>
+                <router-link to="/">
+                    Movies
+                </router-link>
+            </li>
+            <li>
+                <router-link to="/actors">
+                    Actors
+                </router-link>
+            </li>
+            <li>
+                <router-link to="/profile">
+                    Profile
+                </router-link>
+            </li>
+        </ul>
     </div>
 </template>
