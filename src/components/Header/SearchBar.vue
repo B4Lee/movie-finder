8f44b4c98de38d247b224f9930a73fcf38d4ee80
B4Lee <iqbalanggoro48@gmail.com>
2023-04-05 00:58:34 +0700

    add SearchBar result function

--- src/components/Header/SearchBar.vue
+++ src/components/Header/SearchBar.vue
@@ -1,10 +1,21 @@
 <template>
     <div class="mt-5 flex">
-       <input 
-       type="text" 
-       class="rounded-full bg-gray-500 px-5 w-50 h-10 mr-3 focus:outline-none" 
-       placeholder="Search...">
-
-       <img src="../../assets/images/avatar.jpg" alt="" class="h-10">
+        <input 
+        type="text" 
+        class="rounded-full bg-gray-500 px-5 w-50 h-10 mr-3 focus:outline-none" 
+        placeholder="Search...">
+        
+        <div v-if="false" class="absolute mt-12 ml-2 rounded bg-gray-600 w-60 ">
+            <ul class="mt-3">
+                <li class="flex items-center border-b border-gray-500 p-2">
+                    <img 
+                    class="w-10 p-1"
+                    src="../../assets/images/yorushika.jpg" alt="">
+                    <span class="ml-3">Die Hard</span>
+                </li>
+            </ul>
+        </div>
+        
+        <img src="../../assets/images/avatar.jpg" alt="" class="h-10">
     </div>
 </template>
