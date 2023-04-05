b970e0cdfcc649340017b1db4d17511b37c4dc24
B4Lee <iqbalanggoro48@gmail.com>
2023-04-05 17:18:27 +0700

    add Global TMDB API Axios

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
 import './assets/main.css'
+import api from './services/api'
 
 const app = createApp(App)
+
+app.config.globalProperties.$http = api;
 
 app.use(router)
 
