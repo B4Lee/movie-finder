e867e423c38e7e9e9db9a3978a91ca9863dc1eb3
B4Lee <iqbalanggoro48@gmail.com>
2023-04-05 17:19:51 +0700

    fetchAPI PopularMovies & MovieItem

--- src/components/Movies/MovieItem.vue
+++ src/components/Movies/MovieItem.vue
@@ -1,15 +1,53 @@
 <template>
     <div>
         <router-link to="/movie/11">
-            <img src="../../assets/images/yorushika.jpg" 
+            <img 
+            :src="posterPath" 
             class="hover:opacity-75 transition easy-in-out duration-150" 
             alt=""/>
         </router-link>
         
-        <h3>Title</h3>
+        <h3>{{movie.title}}</h3>
         <div class="flex">
-            <span class="ml-5"> 48% | 2023-04-03</span>
+            <span class="ml-5"> {{movie.vote_average * 10}}% | {{movie.release_date}}</span>
         </div>
-        <span>Music, Art</span>
+        <span class="text-sm text-gray-500">
+            <span :key="genre" v-for="(genre, index) in movie.genre_ids">
+                {{ genreTypeName(genre, index) }}
+            </span>
+        </span>
     </div>
 </template>
+
+<script>
+export default {
+    props: {
+        movie: {
+            required: true,
+        },
+        genres: {
+            required: true,
+        },
+    },
+    
+    computed: {
+        posterPath() {
+            return "https://image.tmdb.org/t/p/w500/" + this.movie.poster_path;
+        },
+    },
+    
+    methods: {
+        genreTypeName(genraId, index) {
+            for (const item of this.genres) {
+                if (item.id == genraId) {
+                    if (this.movie.genre_ids.length - 1 == index) {
+                        return item.name;
+                    } else {
+                        return item.name + ",";
+                    }
+                }
+            }
+        },
+    },
+};
+</script>
--- src/components/PopularMovies.vue
+++ src/components/PopularMovies.vue
@@ -4,15 +4,55 @@
             Popular Movies
         </h2>
         <div class="grid sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 gap-4">
-            <MovieItem />
-            <MovieItem />
-            <MovieItem />
-            <MovieItem />
+            <MovieItem 
+            :key="movie.id" 
+            v-for="movie in movies" 
+            :movie="movie"
+            :genres="genres"
+            />
+            
         </div>
     </div>
 </template>
 
-<script setup>
+<script>
 import MovieItem from "./Movies/MovieItem.vue"
 
+export default {
+    components: {
+        MovieItem,
+    },
+    
+    data: function(){
+        return {
+            movies: [],
+            genres: []
+        }
+    },
+    
+    async mounted() {
+        
+        this.fetchGenres()
+        
+        try {
+            const response = await this.$http.get('/movie/popular')
+            this.movies = response.data.results;
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    
+    methods: {
+        async fetchGenres() {
+            // https://api.themoviedb.org/3/genre/movie/list
+            try {
+                const response = await this.$http.get('/genre/movie/list')
+                this.genres = response.data.genres;
+            } catch (error) {
+                console.log(error)
+            }
+            
+        }
+    }
+}
 </script>
