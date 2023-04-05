3dac919fa3e16d8d568dbac161042f7fc177aa3a
B4Lee <iqbalanggoro48@gmail.com>
2023-04-06 00:45:46 +0700

    Update router link

--- src/components/Movies/MovieItem.vue
+++ src/components/Movies/MovieItem.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <router-link to="/movie/11">
+        <router-link :to="`/movie/${movie.id}`">
             <img 
             :src="posterPath" 
             class="hover:opacity-75 transition easy-in-out duration-150" 
