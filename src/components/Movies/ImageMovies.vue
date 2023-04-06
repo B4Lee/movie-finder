3e42589d4759eb39fa27e4f4a5abc7c04dc05b11
B4Lee <iqbalanggoro48@gmail.com>
2023-04-07 00:36:24 +0700

    Update ImageMovies.vue

--- src/components/Movies/ImageMovies.vue
+++ src/components/Movies/ImageMovies.vue
@@ -1,22 +1,32 @@
 <template>
     <div class="container mx-auto border-b border-gray-600 lg:px-24 py-4">
-        <h2 class="text-4xl font-semibold mb-5">
-            Images
-        </h2>
-
+        <h2 class="text-4xl font-semibold mb-5"> Images</h2>
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-            <div class="mr-2">
-                <img class="hover:opacity-75 transition easy-in-out duration-150" src="https://image.tmdb.org/t/p/w300//kzIiKLccwuzD3qD5OHFyPIkiqEw.jpg" alt="">
-               <span class="text-gray-300">N-Buna / Yorushika</span>
-            </div>
-            <div class="mr-2">
-                <img class="hover:opacity-75 transition easy-in-out duration-150" src="https://image.tmdb.org/t/p/w300//kzIiKLccwuzD3qD5OHFyPIkiqEw.jpg" alt="">
-               <span class="text-gray-300">N-Buna / Yorushika</span>
-            </div>
-            <div class="mr-2">
-                <img class="hover:opacity-75 transition easy-in-out duration-150" src="https://image.tmdb.org/t/p/w300//kzIiKLccwuzD3qD5OHFyPIkiqEw.jpg" alt="">
-               <span class="text-gray-300">N-Buna / Yorushika</span>
+            <div :key="index" v-for="(image, index) in images" class="mr-2 mt-2">
+                <img 
+                class="hover:opacity-75 transition easy-in-out duration-150" 
+                :src="movieImage(image)">
             </div>
         </div>
     </div>
 </template>
+
+<script>
+export default {
+    props: {
+        images: {
+            required: true,
+        }
+    },
+    
+    methods: {
+        movieImage(image) {
+            if(image.file_path) {
+                return "https://image.tmdb.org/t/p/w300/" + image.file_path;
+            } else {
+                return "https://via.placeholder.com/300x450";
+            }
+        }
+    }
+}
+</script>
